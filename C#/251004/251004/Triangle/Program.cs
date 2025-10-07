79d11a559b99467eaa2db910e27b33d215652695
boricsk <borics.krisztian@gmail.com>
2025-10-07 14:23:11 +0200

    251007

---
+++ C#/251004/251004/Triangle/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle();
+        }
+
+        static void Triangle()
+        { 
+            int n = 0;
+            do
+            {
+                Console.WriteLine("n= ");
+                n = Convert.ToInt32 (Console.ReadLine());
+            } while (n < 1);
+
+            Console.WriteLine();
+
+            //for (int i = 1; i < n + 1; i = i + 2)
+            //{
+            //    Console.Write($"{i} ");
+            //}
+
+            Console.WriteLine();
+
+            for (int i = 1; i < n + 1; i++)
+            {
+                for (int j = n - i; j >= 0; j--)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    if (i <= 9) { Console.Write($"0{i}"); }
+                    else
+                    {
+                        Console.Write($"{i}");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

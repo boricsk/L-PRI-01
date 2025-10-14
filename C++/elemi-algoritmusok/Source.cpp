98fe51b51e991b491e9e3a0e35d4cb2c2f36e7f1
boricsk <borics.krisztian@gmail.com>
2025-10-14 19:59:55 +0200

    251014

--- C++/elemi-algoritmusok/Source.cpp
+++ C++/elemi-algoritmusok/Source.cpp
@@ -6,7 +6,7 @@ int main()
 	int tomb[15] = { 1,34,56,4,57,45,67,6,43,32,33,99,33,100,1 };
 
 	//----------------------------------------------
-	cout << "Minimum maximum kiválasztás" << endl;
+	cout << "Minimum maximum kivalasztas" << endl;
 	/*
 	Bemenet: tomb[0..14] //int : i //int : min := 1 // int
 	Kimenet: tomb[min]
@@ -41,7 +41,7 @@ int main()
 		Ciklus vége
 	Algoritmus vége
 	*/
-	cout << "Kiválogatás. Mennyi 4-es vagy jobb (75-nél nagyobb) van?" << endl;
+	cout << "Kivalogatas. Mennyi 4-es vagy jobb (75-nél nagyobb) van?" << endl;
 	int segedIndex = 0, seged[15];
 	i = 0;
 	for (i = 0; i < 15; i++)
@@ -51,7 +51,7 @@ int main()
 			seged[segedIndex++] = i + 1;
 		}
 	}
-	cout << "Négyes vagy jobb jegy: " << segedIndex << " db, helye(i): " << endl;
+	cout << "Negyes vagy jobb jegy: " << segedIndex << " db, helye(i): " << endl;
 	for (i = 0; i < segedIndex; i++)
 	{
 		cout << seged[i] << " ";
@@ -71,7 +71,7 @@ int main()
 		Kiir: megfelelt
 	Algoritmus vége
 	*/
-	cout << "Megszámlálás. Hányan feleltek meg? (50-nél nagyobb)" << endl;
+	cout << "Megszamlalas. Hanyan feleltek meg? (50-nel nagyobb)" << endl;
 	int megfelelt = 0;
 	for (i = 0; i < 15; i++)
 	{
@@ -100,7 +100,7 @@ int main()
 	tombhossz = sizeof(tomb) / sizeof(tomb[0]);
 
 	cout << "Linearis kereses. " << endl;
-	cout << "Kérek egy számot: "; cin >> keresendo;
+	cout << "Kerek egy szamot: "; cin >> keresendo;
 
 	while (i < tombhossz && tomb[i] != keresendo) { i++; }
 
@@ -108,8 +108,9 @@ int main()
 		cout << "A keresett pontszam : " << keresendo << " index :" << i + 1 << endl;
 	}
 	else {
-		cout << "Nincs ilyen pontszám!" << endl;
+		cout << "Nincs ilyen pontszam!" << endl;
 	}
+	cout << endl;
 
 	//----------------------------------------------
 	//Kiválasztás. Melyik helyen van az 57 pontos dolgozat
@@ -123,10 +124,11 @@ int main()
 		Kiir: i + 1
 	Algoritmus vége
 	*/
-	cout << "Kiválasztás. Hol van az 57 érték" << endl;
+	cout << "Kivalasztas. Hol van az 57 ertek" << endl;
 	i = 0;
 	while (tomb[i] != 57) { i++; }
 	cout << "Az 57. pont a " << i + 1 << "helyen van." << endl;
+	cout << endl;
 
 	//----------------------------------------------
 	//Eldöntés. Van-e max pontszám
@@ -141,13 +143,13 @@ int main()
 
 	Algoritmus vége
 	*/
-	cout << "Eldöntés. Van-e 100 pontos ZH" << endl;
+	cout << "Eldontes. Van-e 100 pontos ZH" << endl;
 	i = 0;
 	while (tomb[i] != 100) { i++; }
 
 	if (i < 15) { cout << "Van"; }
 	else { cout << "Nincs"; }
-
+	cout << endl;
 
 	//----------------------------------------------
 	//Összegzés. Csoportátlag
@@ -163,7 +165,7 @@ int main()
 	Algoritmus vége
 	*/
 
-	cout << "Összegzés. Csoportátlag" << endl;
+	cout << "Osszegzes. Csoportatlag" << endl;
 	i = 0;
 	int sum = 0;
 	for (i = 0; i< 15; i++)
@@ -171,8 +173,14 @@ int main()
 		sum = sum + tomb[i];
 	}
 
-	cout << "Az átlag " << (float)sum / i; 
-	
+	cout << "Az atlag " << (float)sum / i; 
+	cout << endl;
+
+
+	string osztaly[20] = {
+		"Nagy Laci"
+	};
+	cout << osztaly[0];
 	return 0;
 
 }

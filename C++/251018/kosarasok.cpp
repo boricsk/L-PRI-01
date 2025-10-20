7b3ac6a5fd7b51891e193185ba862adb7b3de2b6
boricsk <borics.krisztian@gmail.com>
2025-10-20 10:15:02 +0200

    251020

--- C++/251018/kosarasok.cpp
+++ C++/251018/kosarasok.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 /// <summary>
@@ -21,11 +22,78 @@ struct kosaras
 	int pont;
 };
 
+void kiirat(kosaras csapat[15], int letszam);
+float atlagotszamol(kosaras csapat[15], int letszam);
+string legmagasabb(kosaras csapat[15], int letszam);
+int db50pontfelett(kosaras csapat[15], int letszam);
+
 kosaras csapat[15];
 
 void kmain()
 {
+	int letszam = 0;
 	srand((unsigned int)time(NULL));
+	do {
+		cout << "Kerem a csapat letszamot :"; cin >> letszam;
+		cin.ignore();
+	} while (letszam < 5 || letszam > 12);
+	
+	cout << endl;
+
+	for (int i = 0; i < letszam; i++)
+	{
+		cout << "Kerem a(z) " << i + 1 << " jatekos nevet ";
+		cin >> csapat[i].nev;
+		csapat[i].pont = rand() % 101;
+		csapat[i].mag = rand() % (220 - 170) + 170;
+	}
+
+	kiirat(csapat, letszam);
+	cout << endl;
+	cout << "A pontatlag :" << atlagotszamol(csapat, letszam) << endl;
+	cout << "A legmagasabb jatekos :" << legmagasabb(csapat, letszam) << endl;
+	cout << "50 pont felettiek szama :" << db50pontfelett(csapat, letszam) << endl;
+
 
 	cout << "Hihi";
+}
+
+void kiirat(kosaras csapat[15], int letszam)
+{
+	system("cls");
+	cout.setf(ios::left);
+	for (int i = 0; i < letszam; i++)
+	{
+		cout << setw(25) << "Nev : " << csapat[i].nev << setw(10) << " Mag. : " << csapat[i].mag << setw(10) << " Pont : " << csapat[i].pont << endl;
+	}
+}
+
+float atlagotszamol(kosaras csapat[15], int letszam)
+{
+	int atlag = 0;
+	for (int i = 0; i < letszam; i++)
+	{
+		atlag = atlag + csapat[i].pont;
+	}
+	return (float)atlag / letszam;
+}
+
+string legmagasabb(kosaras csapat[15], int letszam)
+{
+	int maxindex = 0;
+	for (int i = 0; i < letszam; i++)
+	{
+		if (csapat[maxindex].mag < csapat[i].mag) { maxindex = i; }
+	}
+	return csapat[maxindex].nev;
+}
+
+int db50pontfelett(kosaras csapat[15], int letszam)
+{
+	int db = 0;
+	for (int i = 0; i < letszam; i++)
+	{
+		if (csapat[i].pont > 50) { db++; }
+	}
+	return db;
 }
